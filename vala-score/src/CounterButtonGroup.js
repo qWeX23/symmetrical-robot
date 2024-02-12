25e8344ee24ecc32d65baf8f6ad8479509a33aa0
Benjamin Churchill <bjc9001@gmail.com>
2024-02-12 12:53:39 -0600

    updates

--- vala-score/src/CounterButtonGroup.js
+++ vala-score/src/CounterButtonGroup.js
@@ -1,21 +1,73 @@
-// CounterButtonGroup.js
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './CounterButtonGroup.css'
 
-function CounterButtonGroup({ value, setValue, id, label }) {
-  return (<div>
-    <label htmlFor={id}>{label}</label>
-    <div className='form-group'>
-        
-        <button className='plus-button' onClick={() => setValue(value + 1)}>+</button>
-        {/* <input id={id} type="number" value={value} onChange={(e) => setValue(parseInt(e.target.value))} /> */}
-        <div className='counter-value'>{value}</div>
-        <button className='minus-button' onClick={() => setValue(value - 1)}>-</button>
+const INTERVAL_DELAY = 150;
 
+function CounterButtonGroup({ value, setValue, id, label }) {
+  const [intervalId, setIntervalId] = useState(null);
+  const isMounted = useRef(false);
+
+  useEffect(() => {
+    isMounted.current = true;
+    return () => {
+      isMounted.current = false;
+    }
+  }, []);
+
+  const startIncrement = () => {
+
+    if (isMounted.current) {
+      clearInterval(intervalId);
+      setIntervalId(
+        setInterval(() => {
+          setValue(prevValue => prevValue + 1)
+      }, INTERVAL_DELAY));
+    }
+  };
+
+  const startDecrement = () => {
+    if (isMounted.current) {
+      clearInterval(intervalId);
+      setIntervalId(
+        setInterval(() => {
+          setValue(prevValue => prevValue - 1)
+        }, INTERVAL_DELAY));
+    }
+  };
+
+  const stopChanging = () => {
+    if (isMounted.current) {
+      clearInterval(intervalId);
+    }
+  };
+
+  return (
+    <div>
+      <label htmlFor={id}>{label}</label>
+      <div className='form-group'>
+        <button
+          className='plus-button'
+          onMouseDown={startIncrement}
+          onMouseUp={stopChanging}
+          onMouseLeave={stopChanging}
+          onTouchStart={startIncrement}
+          onTouchEnd={stopChanging}
+        >
+          +
+        </button>
+        <div className='counter-value'>{value}</div>
+        <button
+          className='minus-button'
+          onMouseDown={startDecrement}
+          onMouseUp={stopChanging}
+          onMouseLeave={stopChanging}
+          onTouchStart={startDecrement}
+          onTouchEnd={stopChanging}
+        >
+          -
+        </button>
+      </div>
     </div>
-  </div>
-   
-    
   );
 }
 
